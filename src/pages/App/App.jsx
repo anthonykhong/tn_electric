2f565254d69b6a6b0671eedabb687605e2a8c0a5
anthonykhong <anthonykhong95200@gmail.com>
2023-07-10 13:51:16 -0700

    import home to app file

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
 import "./App.css";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </main>
+  );
 }
