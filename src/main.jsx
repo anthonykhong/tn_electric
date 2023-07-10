f27d2275991b37f4a9b52409b77b62dd781f876d
anthonykhong <anthonykhong95200@gmail.com>
2023-07-10 13:50:39 -0700

    change import in main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./pages/App/App";
-import "./index.css";
+import "./style/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
