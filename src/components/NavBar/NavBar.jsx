c378ef9b09feb6b61a77b0192297ef71fe78e5db
anthonykhong <anthonykhong95200@gmail.com>
2023-07-10 15:36:17 -0700

    add elements to nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import * as userService from "../../utilities/users-service";
 
-export default function NavBar() {
+export default function NavBar({ updateUser }) {
+  function handleLogOut() {
+    userService.logOut();
+    updateUser(null);
+  }
+
   return (
-    <nav>
-      <div>Home</div>
+    <nav className="flex items-center justify-between px-4 py-2 bg-gray-200">
+      <a href="/">
+        <img className="h-8" src="" alt="Logo" />
+      </a>
+      <div className="flex items-center space-x-4">
+        <Link className="" to="/">
+          Home
+        </Link>
+        <Link className="" to="" onClick={handleLogOut}>
+          Log Out
+        </Link>
+      </div>
     </nav>
   );
 }
