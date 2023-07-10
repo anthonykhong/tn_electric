46bd665fcf0f748528c9d6e1bc3f0ae49953a399
anthonykhong <anthonykhong95200@gmail.com>
2023-07-10 14:27:23 -0700

    import pages and components to app

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { getUser } from "../../utilities/users-service";
 import Home from "../Home/Home";
+import Auth from "../Auth/Auth";
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+
 import "./App.css";
 
 export default function App() {
+  const [user, setUser] = useState(getUser());
+
+  function updateUser(userState) {
+    setUser(userState);
+  }
+
   return (
     <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      {user ? (
+        <>
+          <NavBar updateUser={updateUser} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <hr />
+          <Footer />
+        </>
+      ) : (
+        <Auth setUser={setUser} />
+      )}
     </main>
   );
 }
---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Auth() {
+  return (
+    <div>
+      <h1>TN ELECTRIC</h1>
+    </div>
+  );
+}
