25718a73559076cb081e18229cc4af55e3ed4526
anthonykhong <anthonykhong95200@gmail.com>
2023-07-10 15:36:04 -0700

    implement auth page

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import SignUp from "../../components/SignUp/SignUp";
+import Login from "../../components/Login/Login";
 
-export default function Auth() {
+export default function Auth({ setUser }) {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <h1>TN ELECTRIC</h1>
+    <div className="flex flex-col md:flex-row justify-center items-center h-screen">
+      <style>{`body { background-image: url(https://res.cloudinary.com/diw7vmgum/image/upload/v1689027157/wallpaperflare.com_wallpaper_1_xlldyo.jpg); }`}</style>
+      <section className="flex flex-col pr-20">
+        <div className="flex items-center justify-around">
+          <img className="h-10" src="" />
+          &nbsp; &nbsp;
+          <h1 className="text-5xl font-bold text-[#e2be1d]">
+            TN Electric Co. Ltd.
+          </h1>
+        </div>
+        <h2 className="flex justify-center m-10 text-xl text-white">
+          Illuminating Lives with Solar Energy
+        </h2>
+      </section>
+      <div>
+        {show ? <SignUp setUser={setUser} /> : <Login setUser={setUser} />}
+        <p className={`flex justify-center text-white ${show ? "hidden" : ""}`}>
+          Don't have an account yet?{" "}
+          <a className="cursor-pointer" onClick={() => setShow(true)}>
+            <span className=" pl-2 hover:underline">Sign Up</span>
+          </a>
+        </p>
+        <div className="flex justify-center text-white">
+          {show && (
+            <p className="">
+              Already have an account?{" "}
+              <a className="cursor-pointer" onClick={() => setShow(false)}>
+                <span className="hover:underline">Login </span>
+              </a>
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
