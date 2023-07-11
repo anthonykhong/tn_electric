8a5ebd8d479666fca8f806f7a02c022a806d3ef1
anthonykhong <anthonykhong95200@gmail.com>
2023-07-11 13:54:01 -0700

    add placeholder in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="h-screen">
-      <h1 className="text-blue-500">WELCOME</h1>
-    </div>
+    <main className="h-screen">
+      <div className="flex justify-center mt-12">
+        <h1 className="text-[#d4af37] text-2xl font-bold">
+          TN Electric Solar Panels
+        </h1>
+      </div>
+    </main>
   );
 }
