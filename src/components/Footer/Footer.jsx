95dd95d355c818a754e846370f7bf95ac236ade3
anthonykhong <anthonykhong95200@gmail.com>
2023-07-11 13:47:41 -0700

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="flex flex-col footer footer-center p-8 bg-gradient-to-r from-neutral-900 via-neutral-700 to-neutral-900">
+      <div className="text-center text-md text-white">
+        <p>TN Electric Co. LTD.</p>
+        <p>Copyright © 2023 - All rights reserved</p>
+      </div>
+      <div>
+        <div className="flex justify-center items-center pt-2"></div>
+      </div>
+    </footer>
   );
 }
