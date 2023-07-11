bfdf6c7ae211fc883d73edb18b3fe8da63df71a3
anthonykhong <anthonykhong95200@gmail.com>
2023-07-11 13:47:35 -0700

    add screen height to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen">
       <h1 className="text-blue-500">WELCOME</h1>
     </div>
   );
